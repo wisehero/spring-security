7d5dd4bef04ab6ca7bbb9814e25043c3bfa6ee89
wisehero <godjw96@gmail.com>
2022-05-16 12:37:45 +0900

    # Project Setting

---
+++ src/main/java/com/prgrms/devcourse/springsecuritymasterclass/SpringSecurityMasterClassApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.devcourse.springsecuritymasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMasterClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMasterClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prgrms/devcourse/springsecuritymasterclass/SpringSecurityMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.devcourse.springsecuritymasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMasterClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
