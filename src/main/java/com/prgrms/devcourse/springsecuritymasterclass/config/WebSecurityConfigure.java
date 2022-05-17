e14a80ffbce9fd1b95ec5f59f0fdc924e6c3d6e0
wisehero <godjw96@gmail.com>
2022-05-17 11:08:45 +0900

    # 미션 1

--- src/main/java/com/prgrms/devcourse/springsecuritymasterclass/config/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/springsecuritymasterclass/config/WebSecurityConfigure.java
@@ -1,17 +1,19 @@
 package com.prgrms.devcourse.springsecuritymasterclass.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     @Override // WebSecurity 클래스는 필터 체인 관련 전역 설정을 처리할 수 있는 API 제공
-    public void configure(WebSecurity web)  {
+    public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/assets/**");
     }
 
@@ -25,6 +27,25 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/")
-                .permitAll().and();
+                .permitAll().and()
+                .rememberMe().rememberMeParameter("remember-me")
+                .tokenValiditySeconds(300)
+                .and()
+                .logout()// 로그아웃 설정
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .and() // HTTP 요청을 HTTPS 요청으로 리다이렉트
+                .requiresChannel()
+                .anyRequest().requiresSecure();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin123").roles("ADMIN");
     }
 }
